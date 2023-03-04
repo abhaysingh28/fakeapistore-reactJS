623021afcc6f4cdb429fac506bb3de7338ec4e11
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2023-03-04 23:09:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/"  element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/single/:id" element={<Single />} />
         </Routes>
